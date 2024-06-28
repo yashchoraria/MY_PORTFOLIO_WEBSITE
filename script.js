d7caa45dc426fe5e5163fa399fb5099e75f4f923
yashchoraria <yc21chb0a60@student.nitw.ac.in>
2024-06-28 18:37:11 +0530

    the navbars are done for mobile view and desktop view

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const mobileNavbar = document.querySelector('.mobile_navbar');
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.mobile_navbar .nav_links');
+const hamburger = document.querySelector('.hamburger-icon');
+const navLinks = document.querySelector('.nav_link');
 
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');    
     if (navLinks.classList.contains('active')) {
         navLinks.classList.remove('active');
     }
