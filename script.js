5030fd3a2955f9a8911545495d88a8b81de24359
yashchoraria <yc21chb0a60@student.nitw.ac.in>
2024-06-29 13:26:39 +0530

    the home section is ready that is the front look of the page for both desktop and mobile devices are ready

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mobileNavbar = document.querySelector('.mobile_navbar');
 const hamburger = document.querySelector('.hamburger-icon');
 const navLinks = document.querySelector('.nav_link');
+const btn_color1 = document.querySelector('.btn_color1');
+const btn_color2 = document.querySelector('.btn_color2'); 
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');    
@@ -10,4 +12,12 @@ hamburger.addEventListener('click', () => {
     else {
         navLinks.classList.add('active');
     }
+});
+
+btn_color1.addEventListener('click', () => {
+    window.open('./assets/Yash_Choraria_Resume.pdf', '_blank');
+});
+
+btn_color2.addEventListener('click', () => {
+    window.location.href = '.#contact'; 
 });
